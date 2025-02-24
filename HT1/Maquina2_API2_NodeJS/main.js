61d5d7577ed4ebbd96c3e7ee4c6f3e666cff2400
Diego Facundo Pérez Nicolau <3607834730101@ingenieria.usac.edu.gt>
2025-02-23 20:25:01 -0600

    Created API2 with nodeJS

---
+++ HT1/Maquina2_API2_NodeJS/main.js
@@ -0,0 +1,23 @@
+/*
+npm init -y
+npm install express
+*/
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/info', (req, res) => {
+    res.json({ "Instancia": "Maquian 2 - API 2",
+    "Curso": "Seminario de Sistemas 1",
+    "Grupo": "Grupo 12" });
+});
+
+app.get('/check', (req, res) => {
+    res.status(200).send('OK');
+});
+
+// Se corre con node main.js
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
